db841cc7abf9799dd93aca07f6cf292f4d85ecd9
tjd985 <tjd985@gmail.com>
2024-07-29 16:28:08 +0900

    ✨ [Feat] Header 메뉴 상수 모듈화
    메뉴 상수 모듈화

---
+++ src/constants/constants.ts
@@ -0,0 +1,18 @@
+type Header = {
+  [key: string]: string[];
+};
+
+const HEADER: Header = {
+  MENU_LIST: [
+    "올림픽 미리보기",
+    "아울렛",
+    "브랜드",
+    "여성",
+    "남성",
+    "골프",
+    "아웃도어 & 스포츠",
+  ],
+  GROUP_LIST: ["런칭", "베스트", "스타일", "기획전", "이벤트"],
+};
+
+export default HEADER;
