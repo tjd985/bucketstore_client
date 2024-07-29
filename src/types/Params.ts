d4d7d5ab2b471e4eedc598f97f791a6ef3090a6e
tjd985 <tjd985@gmail.com>
2024-07-29 22:23:49 +0900

    🏷️ [Type] 타입 정의
    API Param, API응답 타입 정의

---
+++ src/types/Params.ts
@@ -0,0 +1,8 @@
+type Type =
+  | "newest"
+  | "low_price"
+  | "high_price"
+  | "high_discount_rate"
+  | "best";
+
+export default Type;
---
+++ src/types/Product.ts
@@ -0,0 +1,36 @@
+type Price = {
+  tag: number;
+  real: number;
+  discountRate: number;
+  maxDiscountPrice: number;
+  maxDiscountRate: number;
+};
+
+type Badge = {
+  goodsCode: string;
+  id: number;
+  type: string;
+  name: string;
+  inOrder: number;
+  isAuto: boolean;
+};
+
+type Product = {
+  code: string;
+  name: string;
+  statusCode: string;
+  brandName: string;
+  brandId: number;
+  price: Price;
+  url: string;
+  isFavorite: boolean;
+  isSoldOut: boolean;
+  deliveryFeeCode: string;
+  badges: {
+    custom: Badge[];
+    attribute: Badge[];
+    recommend: Badge[];
+  };
+};
+
+export default Product;
