f163a14b7a672513221b4fcd9be4809012342307
tjd985 <tjd985@gmail.com>
2024-07-29 20:31:55 +0900

    ♻️ [Refactor] SIDEBAR 상수 추가
    사이드바 상수 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,8 +1,8 @@
-type Header = {
+type Constant = {
   [key: string]: string[];
 };
 
-const HEADER: Header = {
+const HEADER: Constant = {
   MENU_LIST: [
     "올림픽 미리보기",
     "아울렛",
@@ -15,4 +15,18 @@ const HEADER: Header = {
   GROUP_LIST: ["런칭", "베스트", "스타일", "기획전", "이벤트"],
 };
 
-export default HEADER;
+const SIDEBAR: Constant = {
+  CATEGORY_LIST: [
+    "전체",
+    "집업",
+    "긴팔",
+    "반팔",
+    "민소매",
+    "긴팔이너",
+    "반팔이너",
+    "셔츠",
+  ],
+  FILTER_LIST: ["브랜드", "시즌", "추천", "색상", "혜택", "가격", "할인율"],
+};
+
+export { HEADER, SIDEBAR };
