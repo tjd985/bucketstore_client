8c284194399c59a60f699ee39bd4ccb997305293
tjd985 <tjd985@gmail.com>
2024-07-30 22:25:43 +0900

    ✨ [Feat] Product 컴포넌트 구현
    카드가 렌더링 될 Product컴포넌트 구현

---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { nanoid } from "nanoid";
+
+import Card from "./Card/Card.tsx";
+import Loading from "../shared/Loading.tsx";
+
+import useProductData from "../../hooks/useProductData.ts";
+
+import Type from "../../types/Params.ts";
+import Badge from "../../types/Product.ts";
+
+function Product() {
+  const [type, setType] = useState<Type>("newest");
+  const [page, setPage] = useState(1);
+  const [products, errorMessage, isLoading, isError] = useProductData(
+    type,
+    page,
+  );
+
+  if (isLoading) {
+    return (
+      <ProductLayout>
+        <Loading />
+      </ProductLayout>
+    );
+  }
+
+  return (
+    <ProductLayout>
+      <CardContainer>
+        {products.map((product: Badge) => {
+          const { url, name, price } = product;
+          const { real, tag } = price;
+
+          return (
+            <Card
+              key={nanoid(10)}
+              imagePath={url}
+              name={name}
+              tagPrice={tag}
+              sellPrice={real}
+            />
+          );
+        })}
+      </CardContainer>
+    </ProductLayout>
+  );
+}
+
+const ProductLayout = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  flex-shrink: 1;
+  flex-grow: 1;
+  width: 100%;
+  height: calc(100vh - 127px);
+  padding: 0 20px;
+`;
+
+const CardContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, minmax(0px, 1fr));
+  gap: 80px 1px;
+`;
+
+export default Product;
