e62277cba3a041c14f6a0664adbf972519dc43a5
tjd985 <tjd985@gmail.com>
2024-07-30 22:25:07 +0900

    ✨ [Feat] Card 컴포넌트 구현
    Product Card 컴포넌트 구현

---
+++ src/components/Product/Card/Card.tsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+type CardParams = {
+  imagePath: string;
+  name: string;
+  tagPrice: number;
+  sellPrice: number;
+};
+
+function Card({ imagePath, name, tagPrice, sellPrice }: CardParams) {
+  return (
+    <CardWrapper>
+      <ImageSection className="image_section">
+        <img src={imagePath} alt={`${name}이미지`} width="100%" />
+      </ImageSection>
+      <DescriptionSection className="description_section">
+        <h1 className="product_name">{name}</h1>
+        <section className="price_section">
+          <span className="sell_price">{sellPrice.toLocaleString()}</span>
+          <span className="tag_price">{tagPrice.toLocaleString()}</span>
+        </section>
+      </DescriptionSection>
+    </CardWrapper>
+  );
+}
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  cursor: pointer;
+`;
+
+const ImageSection = styled.section`
+  width: 100%;
+`;
+
+const DescriptionSection = styled(ImageSection)`
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: column;
+  padding: 5px 0;
+
+  > * {
+    margin-top: 10px;
+
+    font-size: 0.9rem;
+  }
+
+  .product_name {
+    text-overflow: ellipsis;
+  }
+
+  .sell_price {
+    margin-right: 5px;
+
+    font-weight: bold;
+  }
+
+  .tag_price {
+    color: #bbbbbb;
+  }
+`;
+
+export default Card;
