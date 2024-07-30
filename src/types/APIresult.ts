93fa8796698dcb0cb462c93ab18cab828cd63122
tjd985 <tjd985@gmail.com>
2024-07-30 22:52:13 +0900

    🏷️ [Type] Meta타입 추가
    API응답을 위한 Meta타입 추가

--- src/types/APIresult.ts
+++ src/types/APIresult.ts
@@ -1,9 +1,22 @@
-import Badge from "./Product.ts";
+import Product from "./Product.ts";
+
+type Meta = {
+  checksum: number | null;
+  pageInfo: {
+    page: number;
+    pages: number;
+    size: number;
+    total: number;
+  };
+};
 
 type APIresult = {
   result: string;
   status: string;
-  content?: { body: Badge[] };
+  content?: {
+    meta: Meta;
+    body: Product[];
+  };
   message?: string;
 };
 
