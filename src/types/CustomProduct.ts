30e98f27d006d16b29182197099d478e211556e0
tjd985 <tjd985@gmail.com>
2024-08-01 13:58:49 +0900

    🏷️ [Type] 타입 추가
    Card렌더링에 필요한 정보만 타입 따로 정의

---
+++ src/types/CustomProduct.ts
@@ -0,0 +1,10 @@
+type CustomProduct = {
+  imagePath: string;
+  name: string;
+  code: string;
+  tagPrice: number;
+  realPrice: number;
+  badgeNameList: string[];
+};
+
+export default CustomProduct;
