cd1f31643b0826ce04abdee23ff8510387a99233
tjd985 <tjd985@gmail.com>
2024-08-02 02:51:06 +0900

    ✨ [Feat] TopScrollButton 구현
    클릭시 최상단으로 이동하는 버튼 구현

---
+++ src/components/shared/TopButton.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { IoIosArrowRoundUp } from "react-icons/io";
+
+import useScrollStore from "../../store/scroll.ts";
+
+function TopButton() {
+  const { scroll } = useScrollStore();
+
+  return (
+    <StyledTopButton
+      className={scroll === "UP" ? "SHOW" : "HIDE"}
+      onClick={() => {
+        window.scroll({ top: 0, behavior: "smooth" });
+      }}
+    >
+      <IoIosArrowRoundUp className="top_arrow icon" />
+    </StyledTopButton>
+  );
+}
+
+const StyledTopButton = styled.button`
+  position: fixed;
+  bottom: 80px;
+  right: 20px;
+
+  z-index: 10;
+  background-color: transparent;
+  border: none;
+  transition-duration: 0.3s;
+
+  cursor: pointer;
+
+  &.HIDE {
+    transform: translateY(calc(100% + 80px));
+  }
+
+  .top_arrow {
+    display: flex;
+    width: 50px;
+    height: 50px;
+    border: 1px solid #ccc;
+    border-radius: 50px;
+
+    background-color: #fff;
+    opacity: 0.9;
+  }
+`;
+
+export default TopButton;
