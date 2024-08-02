c911b1c31ab3b2679fb94fc1f662c4a8e8e1904c
tjd985 <tjd985@gmail.com>
2024-08-02 20:08:24 +0900

    📦️ [Chore] test config 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    collectCoverageFrom: ["src/**/*.tsx", "src/**/*.ts"],
+    setupFiles: "src/spec/setupTests.ts",
+    testMatch: ["./src/spec/*.spec.tsx"],
+    environment: "jsdom",
+    coverage: {
+      all: true,
+      exclude: ["*.config.ts", "*.cjs", "**/main.tsx", "**/constants.ts"],
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "./coverage",
+    },
+  },
 });
